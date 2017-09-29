58caed0d57d9e69fbe05e728d383ca9250427390
Hamza Ghani <h.u.f.ghani@gmail.com>
2017-09-29 18:09:56 +0100

    m flag

--- CW1/miniwc.py
+++ CW1/miniwc.py
@@ -6,8 +6,14 @@ import sys
 def read_file(file_name):
     data = ""
     if len(files) > 0:
-        f = open(file_name, "r")
-        data = f.read()
+        try:
+            f = open(file_name, "r")
+            data = f.read()
+        except IOError:
+            print ("Error: File does not appear to exist.")
+            return 0
+        finally:
+            f.close()
     return data
 
 
@@ -31,15 +37,23 @@ def character_count(file_name):
 def byte_count(file_name):
     byte = 0
     if len(file_name) > 0:
-        byte = len(file_name.encode('utf-8'))+1
+        byte = len(file_name.encode('utf-8'))
     return byte
 
 
 if __name__ == '__main__':
 
     files = list(map(str, sys.argv[1:]))
-
-    for file in files:
-        temp_data = read_file(file)
-        print("       " + str(count_lines(temp_data)) + "       " + str(count_words(temp_data)) + "      "
-              + str(byte_count(temp_data))+" "+file)
+    option = ""
+    if files.__getitem__(0).__contains__("-m"):
+        option = files.__getitem__(0)
+        del files[0]
+        for file in files:
+            temp_data = read_file(file)
+            print("       " + str(count_lines(temp_data)) + "       " + str(count_words(temp_data)) + "      "
+                  + str(byte_count(temp_data)) + "       " + str(character_count(temp_data)) + " " + file)
+    else:
+        for file in files:
+            temp_data = read_file(file)
+            print("       " + str(count_lines(temp_data)) + "       " + str(count_words(temp_data)) + "      "
+                  + str(byte_count(temp_data)) + " " + file)
